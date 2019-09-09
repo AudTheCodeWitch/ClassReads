a282baca23801119460019ce55040e81576b8911
Audrea Cook <audreacwork@gmail.com>
2019-09-09 15:52:04 -0600

    Build seed file

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+Teacher.all.each do |t|
+  # create book library
+  100.times do
+    b = Faker::Book
+    Book.create(title: b.title, author: b.author, genre: b.genre, teacher_id: t.id)
+  end
+
+  # create students
+  20.times do
+    first = Faker::Name.first_name
+    last = Faker::Name.last_name
+    name = "#{first} #{last}"
+    username = first[0] + last
+    pass = Faker::Creature::Animal
+    Student.create(name: name, username: username.downcase, password: pass.name, teacher_id: t.id)
+  end
+end
+
+# Create book reviews
+Student.all.each do |s|
+  5.times do
+    rating = Faker::Number.within(range: 1..5)
+    review = Faker::Movies::HarryPotter.quote
+    book = Book.all[rand(Book.all.length)]
+    Review.create(rating: rating, review: review, student_id: s.id, book_id: book.id)
+  end
+end
