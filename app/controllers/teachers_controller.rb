f424f9ca40bc36efb42c78f914b8711fca0f3a31
Audrea Cook <audreacwork@gmail.com>
2019-09-10 11:48:06 -0600

    Build get '/teachers/:username' route

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,1 +1,10 @@
-
+class TeachersController < ApplicationController
+  get 'teachers/:username' do
+    if logged_in?
+      @teacher = Teacher.find_by_id(params[:id])
+      erb :'teacher/index'
+    else
+      redirect '/login'
+    end
+  end
+end
