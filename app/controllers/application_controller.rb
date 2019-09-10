02eddf1918726aa250e7714f45d10636e4cb04f4
Audrea Cook <audreacwork@gmail.com>
2019-09-10 11:41:54 -0600

    Build post '/login' logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,21 @@ class ApplicationController < Sinatra::Base
     erb :register
   end
 
+  post '/login' do
+    if params[:username] == "" || params[:name] == "" || params[:password] == ""
+      redirect '/register'
+    elsif params[:role] == "teacher"
+      @user = Teacher.create(name: params[:name], username:params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/students'
+    elsif params[:role] == 'student'
+      @user = Student.create(name: params[:name], username:params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/reviews'
+
+    end
+  end
+
   helpers do
     def logged_in?
       !!session[:user_id]
