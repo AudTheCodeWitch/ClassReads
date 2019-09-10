c04401541298bd6cbfd29f67041d9b30ffe16b68
Audrea Cook <audreacwork@gmail.com>
2019-09-10 15:55:52 -0600

    Stub CRUD routes in StudentsController

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,12 @@
 class StudentsController < ApplicationController
+  get '/students' do
+    erb :'student/index'
+  end
+
+  get '/students/new' do
+    erb :'student/new'
+  end
+
   get '/students/:username' do
     if logged_in?
       @student = Student.find_by(username: params[:username])
@@ -7,4 +15,19 @@ class StudentsController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/students/:username/edit' do
+    erb :'student/edit'
+  end
+
+  patch '/students/:username' do
+    redirect '/students/:username'
+  end
+
+  delete '/students/:username' do
+    @student = Student.find_by(username: params[:username])
+    @stuent.delete
+    redirect '/students'
+  end
+
 end
--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@ require './config/environment'
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
+use Rack::MethodOverride
 use TeachersController
 use StudentsController
 run ApplicationController
