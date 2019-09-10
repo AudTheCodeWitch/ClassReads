f689170770b56d8fc3ee502c34241f6953caa54a
Audrea Cook <audreacwork@gmail.com>
2019-09-10 15:38:22 -0600

    Write get '/students/:username' route

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,1 +1,10 @@
-
+class StudentsController < ApplicationController
+  get '/students/:username' do
+    if logged_in?
+      @student = Student.find_by(username: params[:username])
+      erb :'student/show'
+    else
+      redirect '/login'
+    end
+  end
+end
