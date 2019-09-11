8d17fded5ba22fd3cbeb506359becc88bf665797
Audrea Cook <audreacwork@gmail.com>
2019-09-11 11:02:21 -0600

    Add routes to edit and delete teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,4 +8,38 @@ class TeachersController < ApplicationController
     end
   end
 
+  get'/teachers/:username/edit' do
+    if current_user.username == params[:username]
+      @teacher = Teacher.find_by(username: params[:username])
+      erb :'teacher/edit'
+    else
+      redirect '/teachers/:username'
+    end
+  end
+
+  patch '/teachers/:username' do
+
+  end
+
+  get '/teachers/:username/delete' do
+    if current_user.username == params[:username]
+      @teacher = Teacher.find_by(username: params[:username])
+      erb :'teacher/delete'
+    else
+      redirect '/teachers/:username'
+    end
+  end
+
+  delete '/teachers/:username' do
+    if current_user.username == params[:username]
+      @teacher = Teacher.find_by(username: params[:username])
+      Teacher.destroy(@teacher.id)
+      session.destroy
+      redirect '/'
+    else
+      redirect '/teachers/:username'
+    end
+  end
+
+
 end
