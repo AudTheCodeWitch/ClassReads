e5650a40a0a4e96b7a8334e3774d136e55911a84
Audrea Cook <audreacwork@gmail.com>
2019-09-11 14:44:01 -0600

    Add slug methods for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ActiveRecord::Base
   belongs_to :teacher
   has_many :reviews
   has_many :students, through: :reviews
+
+  def slug
+    title.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Book.all.find { |book| book.slug == slug }
+  end
 end
