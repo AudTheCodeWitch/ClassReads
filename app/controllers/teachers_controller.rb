43fcada1455f87f141de82121b825da1c722ff65
Audrea Cook <audreacwork@gmail.com>
2019-09-11 14:17:28 -0600

    Move @teacher out of if scope Fix redirects

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -9,43 +9,42 @@ class TeachersController < ApplicationController
   end
 
   get'/teachers/:username/edit' do
+    @teacher = Teacher.find_by(username: params[:username])
     if current_user.username == params[:username]
-      @teacher = Teacher.find_by(username: params[:username])
       erb :'teacher/edit'
     else
-      redirect '/teachers/:username'
+      redirect "/teachers/#{@teacher.username}"
     end
   end
 
   patch '/teachers/:username' do
-    if current_user.username == params[:username]
-      @teacher = Teacher.find_by(username: params[:username])
+    @teacher = Teacher.find_by(username: params[:username])
+    if current_user.username == @teacher.username
       @teacher.update(name: params[:name], username: params[:username])
       if params[:password] != ''
         @teacher.update(password: params[:password])
       end
-      redirect "/teachers/#{@teacher.username}"
     end
-
+    redirect "/teachers/#{@teacher.username}"
   end
 
   get '/teachers/:username/delete' do
+    @teacher = Teacher.find_by(username: params[:username])
     if current_user.username == params[:username]
-      @teacher = Teacher.find_by(username: params[:username])
       erb :'teacher/delete'
     else
-      redirect '/teachers/:username'
+      redirect "/teachers/#{@teacher.username}"
     end
   end
 
   delete '/teachers/:username' do
+    @teacher = Teacher.find_by(username: params[:username])
     if current_user.username == params[:username]
-      @teacher = Teacher.find_by(username: params[:username])
       Teacher.destroy(@teacher.id)
       session.destroy
       redirect '/'
     else
-      redirect '/teachers/:username'
+      redirect "/teachers/#{@teacher.username}"
     end
   end
 
