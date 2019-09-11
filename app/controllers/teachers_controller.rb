2487fc3c876dec532b6c039387d2dee6a8622fda
Audrea Cook <audreacwork@gmail.com>
2019-09-11 11:19:01 -0600

    Build patch /teachers/:username logic

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -18,6 +18,14 @@ class TeachersController < ApplicationController
   end
 
   patch '/teachers/:username' do
+    if current_user.username == params[:username]
+      @teacher = Teacher.find_by(username: params[:username])
+      @teacher.update(name: params[:name], username: params[:username])
+      if params[:password] != ''
+        @teacher.update(password: params[:password])
+      end
+      redirect "/teachers/#{@teacher.username}"
+    end
 
   end
 
