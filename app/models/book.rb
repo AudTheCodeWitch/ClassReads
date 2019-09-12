5772996367a9a5287d782e5e7308d6f93b347786
Audrea Cook <audreacwork@gmail.com>
2019-09-12 10:49:08 -0600

    Build average rating method

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,15 @@ class Book < ActiveRecord::Base
   def self.find_by_slug(slug)
     Book.all.find { |book| book.slug == slug }
   end
+
+  def average
+    avg_rating = []
+    if self.reviews.empty?
+      average = "No ratings yet"
+    else
+      self.reviews.each { |r| avg_rating << r.rating }
+      average = avg_rating.sum / avg_rating.length
+    end
+    average
+  end
 end
