319d54e2e9c49cbb83690cfd1ddd7ebb7476f352
Audrea Cook <audreacwork@gmail.com>
2019-09-12 11:44:39 -0600

    Add total_reviews method

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,12 @@ class Teacher < ActiveRecord::Base
   has_secure_password
   has_many :students
   has_many :books
+
+  def total_reviews
+    total = 0
+    self.students.each do |s|
+      total += s.reviews.length
+    end
+    total
+  end
 end
