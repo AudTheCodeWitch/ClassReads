b1ffeae21c9473964c35bb9e7bd699c40ab352cd
Audrea Cook <audreacwork@gmail.com>
2019-09-13 14:11:04 -0600

    Add published_reviews method

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,10 @@ class Student < ActiveRecord::Base
   belongs_to :teacher
   has_many :reviews
   has_many :books, through: :reviews
+
+  def published_reviews
+    published = []
+    self.reviews.each { |r| published << r if r.book_id != nil}
+    published
+  end
 end
