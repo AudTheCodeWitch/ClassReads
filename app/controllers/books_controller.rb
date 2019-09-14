c259745a88f2a648dd5f2951389e3dc190a05b5b
Audrea Cook <audreacwork@gmail.com>
2019-09-14 12:35:22 -0600

    Add :id to book routes to eliminate duplicate book bug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,8 @@
 class BooksController < ApplicationController
   get '/books' do
     if logged_in?
-      if session[:role] == "teacher"
-        @teacher = Teacher.find_by(username: current_user.username)
-      else
-        @student = Student.find_by(username: current_user.username)
-        @teacher = @student.teacher
-      end
+      assign_teacher(current_user)
       @books = @teacher.books.all.sort_by { |b| b.author}
-
       erb :'book/index'
     else
       redirect '/login'
@@ -31,84 +25,84 @@ class BooksController < ApplicationController
     redirect '/books'
   end
 
-  get '/books/:slug' do
+  get '/books/:id/:slug' do
     if logged_in?
-      @book = Book.find_by_slug(params[:slug])
+      @book = Book.find_by_id(params[:id])
       erb :'book/show'
     else
       redirect '/login'
     end
   end
 
-  get '/books/:slug/edit' do
-    @book = Book.find_by_slug(params[:slug])
+  get '/books/:id/:slug/edit' do
+    @book = Book.find_by_id(params[:id])
     if current_user.username == @book.teacher.username
       @teacher = @book.teacher
       erb :'book/edit'
     else
-      redirect "/books/#{@book.slug}"
+      redirect "/books/#{@book.id}/#{@book.slug}"
     end
   end
 
-  patch '/books/:slug' do
-    @book = Book.find_by_slug(params[:slug])
+  patch '/books/:id/:slug' do
+    @book = Book.find_by_id(params[:id])
     @book.update(params[:book])
-    redirect "/books/#{@book.slug}"
+    redirect "/books/#{@book.id}/#{@book.slug}"
   end
 
-  delete '/books/:slug' do
-    @book = Book.find_by_slug(params[:slug])
+  delete '/books/:id/:slug' do
+    @book = Book.find_by_id(params[:id])
     @book.reviews.each {|r| r.destroy}
     @book.save
     @book.destroy
     redirect '/books'
   end
 
-  get '/books/:slug/reviews/new' do
-    @book = Book.find_by_slug(params[:slug])
+  get '/books/:id/:slug/reviews/new' do
+    @book = Book.find_by_id(params[:id])
     if session[:role] == "student"
       erb :'book/review/new'
     else
-      redirect "/books/#{@book.slug}"
+      redirect "/books/#{@book.id}/#{@book.slug}"
     end
   end
 
-  post '/books/:slug/reviews' do
-    @book = Book.find_by_slug(params[:slug])
+  post '/books/:id/:slug/reviews' do
+    @book = Book.find_by_id(params[:id])
     @review = Review.create(rating: params[:review][:rating], review: params[:review][:review])
     @review.student = Student.find_by(username: current_user.username)
     @review.book = Book.find_by_slug(params[:slug])
     @review.save
-    redirect "/books/#{@book.slug}"
+    redirect "/books/#{@book.id}/#{@book.slug}"
   end
 
-  get '/books/:slug/reviews/:id/edit' do
-    @book = Book.find_by_slug(params[:slug])
-    @review = @book.reviews.find_by_id(params[:id])
+  get '/books/:id/:slug/reviews/:review_id/edit' do
+    @book = Book.find_by_id(params[:id])
+    @review = @book.reviews.find_by_id(params[:review_id])
     if current_user.username == @review.student.username || session[:role] == 'teacher'
       erb :'book/review/edit'
     else
-      redirect "/books/#{@book.slug}"
+      redirect "/books/#{@book.id}/#{@book.slug}"
     end
   end
 
-  patch '/books/:slug/reviews/:id' do
-    @book = Book.find_by_slug(params[:slug])
-    @review = @book.reviews.find_by_id(params[:id])
+  patch '/books/:id/:slug/reviews/:review_id' do
+    @book = Book.find_by_id(params[:id])
+    @review = @book.reviews.find_by_id(params[:review_id])
     @student = @review.student
     @review.update(params[:review])
-    redirect "/books/#{@book.slug}"
+    redirect "/books/#{@book.id}/#{@book.slug}"
   end
 
-  delete '/books/:slug/reviews/:id' do
-    @book = Book.find_by_slug(params[:slug])
-    @review = @book.reviews.find_by_id(params[:id])
+  delete '/books/:id/:slug/reviews/:review_id' do
+    @book = Book.find_by_id(params[:id])
+    @review = @book.reviews.find_by_id(params[:review_id])
     @student = @review.student
     if student_user?(@student) || session[:role] == 'teacher'
       Review.destroy(@review.id)
       redirect '/books'
     else
-      redirect "/books/#{@book.slug}"
+      redirect "/books/#{@book.id}/#{@book.slug}"
     end
   end
 
